990335b3ea14e6e667123f4fb3945e108af1277e
HolodTema <art.holodilov@gmail.com>
2025-03-06 08:35:27 +0300

    Unfinished attempt to make QueueList class with doubly linked list.

--- QueueList.h
+++ QueueList.h
@@ -2,38 +2,65 @@
 #define QUEUE_LIST_H
 
 #include "Queue.h"
+#include "queueExceptions.h"
 
 template <class T>
 class QueueList: public Queue<T> {
 public:
-
-    QueueList() {
-
-    }
+    QueueList() = default;
 
     QueueList(const QueueList& other) {
+        if (other.head_ != nullptr) {
+            head_ = new Node(other.head_->data, nullptr, nullptr);
+
+            tail_ = head_;
+            Node* otherNode = other.head_->nextNode_;
+
+            while (otherNode != nullptr) {
+                tail_->nextNode = new Node(otherNode.data_, tail_, nullptr);
+                otherNode = otherNode->nextNode_;
+                node = node->nextNode_;
+            }
+        }
+    }
+
+    QueueList(const QueueList&& other) noexcept:
+        head_(other.head_),
+        tail_(other.tail_)
+    {
 
     }
 
-    QueueList(const QueueList&& other) noexcept {
-
+    ~QueueList() override {
+        if (head_ != nullptr) {
+            while (head_->nextNode_ != nullptr) {
+                const Node* exHead = head_;
+                head_ = head_->nextNode;
+                delete exHead;
+            }
+            delete head_;
+        }
     }
 
     QueueList& operator=(const QueueList& other) {
-
+        if (&other != this) {
+            QueueList& otherCopy(other);
+            swap(otherCopy);
+        }
+        return *this;
     }
 
-    QueueList& operator=(const QueueList&& other) {
-
-    }
-
-    ~Queue {
-
+    QueueList& operator=(QueueList&& other) noexcept {
+        if (&other != this) {
+            head_ = other.head_;
+            tail_ = other.tail_;
+        }
+        return *this;
     }
 
     T deQueue() override {
         if (head_ == nullptr) {
-            throw
+            throw QueueUnderflowException();
         }
         if (head_->nextNode_ == nullptr) {
             T result = head_->data_;
@@ -42,8 +69,7 @@ public:
             return result;
         }
 
-        T newHead = head_
-
+        T newHead = head_;
     }
 
     void enQueue(const T& element) override {
@@ -62,21 +88,26 @@ public:
     bool isEmpty() override {
         return head_ == nullptr;
     }
+
 private:
     class Node {
         T data_;
+        Node* prevNode_;
         Node* nextNode_;
 
-        Node(T data, Node* nextNode):
+        Node(T data, Node* prevNode, Node* nextNode):
             data_(data),
+            prevNode_(prevNode),
             nextNode_(nextNode)
         { }
     };
 
     Node* head_ = nullptr;
+    Node* tail_ = nullptr;
 
     void swap(QueueList& other) noexcept {
-
+        head_ = other.head_;
+        tail_ = other.tail_;
     }
 };
 #endif //QUEUE_LIST_H
--- queueExceptions.h
+++ queueExceptions.h
@@ -10,4 +10,18 @@ public:
     }
 };
 
+class QueueUnderflowException: public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Error: queue underflow.\n";
+    }
+};
+
+class QueueOverflowException: public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Error: queue overflow.\n";
+    }
+};
+
 #endif //QUEUE_EXCEPTIONS_H
